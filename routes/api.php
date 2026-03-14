3ca0fc05cc702097db3e2afa4552da8c41c17c68
4db444 <brahim@gmail.com>
2026-03-14 23:19:10 +0000

    creating the appropriate routes for the favorites functionslity

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ItineraryController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\FavoriteController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,4 +14,15 @@ Route::middleware("auth:sanctum")->group(function () {
     Route::get("/user", [UserController::class, "show"]);
     
     Route::apiResource("/itinerary", ItineraryController::class);
+    Route::prefix("/favorites")->group(function () {
+        Route::get("/", [FavoriteController::class, "index"]);
+        Route::post("/{id}", [FavoriteController::class, "store"]);
+        Route::delete("/{id}", [FavoriteController::class, "destroy"]);
+    });
+});
+
+Route::fallback(function (){
+    return response()->json([
+        "404" => "route does not exists"
+    ]);
 });
